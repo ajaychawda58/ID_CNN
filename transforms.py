14f55517af4855dc70c040d31c80c34266b39096
Ajay Chawda <chawda@rhrk.uni-kl.de>
2021-05-25 20:24:53 +0530

    updated transforms

--- transforms.py
+++ transforms.py
@@ -5,7 +5,6 @@ import torch
 from torchvision.transforms import functional as F
 import numpy as np
 import cv2
-
 def _flip_coco_person_keypoints(kps, width):
     flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
     flipped_data = kps[:, flip_inds]
